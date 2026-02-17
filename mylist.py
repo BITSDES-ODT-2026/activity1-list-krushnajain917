0a08331be9837a01635f1b42ba2426e7faccda45
krushnajain917 <krushnajain917@gmail.com>
2026-02-17 11:47:56 +0530

    Change LED control to use led2, led3, and led4

--- mylist.py
+++ mylist.py
@@ -11,9 +11,9 @@ ml = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
 
 for i in ml:
     led1.value(i[0])
-    led1.value(i[1])
-    led1.value(i[2])
-    led1.value(i[3])
+    led2.value(i[1])
+    led3.value(i[2])
+    led4.value(i[3])
     time.sleep(0.5)
 
 
